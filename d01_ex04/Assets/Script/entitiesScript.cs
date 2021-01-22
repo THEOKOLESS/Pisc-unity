813721d4682bf8517d3efd1f90cdfda1b3ca6a99
Abey Cultivey <AbeyCultivey@MacBook-Pro-de-Josephine.local>
2021-01-22 20:00:58 +0100

    ex04 presque fini

---
+++ d01_ex04/Assets/Script/entitiesScript.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class entitiesScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public static GameObject sTelepIn1;
+    [SerializeField] private GameObject _telep1;
+
+
+    [SerializeField] private GameObject whiteDoor1;
+    [SerializeField] private GameObject redDoor1;
+    [SerializeField] private GameObject yellowDoor1;
+    [SerializeField] private GameObject pushYellow1;
+    [SerializeField] private SpriteRenderer facelessSwitch;
+
+    [SerializeField] private GameObject redDoor2;
+    [SerializeField] private GameObject blueDoor2;
+    [SerializeField] private GameObject yellowDoor2;
+
+    private Vector3 openRedDoor2;
+    private Vector3 openYellowDoor2;
+    private Vector3 openBlueDoor2;
+
+    private Vector3 openWhiteDoor1;
+    private Vector3 openRedDoor1;
+    private Vector3 openYellowDoor1;
+    private Vector3 pushYellow;
+
+    void Start()
+    {
+        sTelepIn1 = _telep1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        if (playerScript_ex04.whiteDoor > 0)
+        {
+            openWhiteDoor1 = new Vector3(-10.65f, -3.77f, 0f);
+            if (whiteDoor1.transform.position.y < openWhiteDoor1.y) {
+                whiteDoor1.transform.Translate(1 * Time.deltaTime * 1.1f, 0, 0);
+            }
+        }
+        if (playerScript_ex04.whiteDoor == 0)
+        {
+            openWhiteDoor1 = new Vector3(-10.65f, -5.44f, 0f);
+            if (whiteDoor1.transform.position.y > openWhiteDoor1.y)
+            {
+                whiteDoor1.transform.Translate(-1 * Time.deltaTime * 1.1f, 0, 0);
+            }
+        }
+
+        if (playerScript_ex04.redDoor1 == true)
+        {   
+            openRedDoor1 = new Vector3(-21.04f, -5.44f, 0f);
+            if (redDoor1.transform.position.x > openRedDoor1.x)
+            {
+                redDoor1.transform.Translate(-1 * Time.deltaTime * 0.1f,  0, 0);
+            }
+        }
+
+        if (playerScript_ex04.yellowDoor1 == true)
+        {
+            openYellowDoor1 = new Vector3(-8.21f, -5.44f, 0f);
+            if (yellowDoor1.transform.position.x < openYellowDoor1.x)
+            {
+                yellowDoor1.transform.Translate(0, -1 * Time.deltaTime * 50f, 0);
+                pushYellow1.transform.Translate(1 * Time.deltaTime * 100f, 0, 0);
+            }
+         }
+        else
+        {
+            openYellowDoor1 = new Vector3(-10.41f, -5.44f, 0f);
+            if (yellowDoor1.transform.position.x > openYellowDoor1.x)
+            {
+                yellowDoor1.transform.Translate(0, 1 * Time.deltaTime * 10f, 0);
+                pushYellow1.transform.Translate(-1 * Time.deltaTime * 20f, 0, 0);
+            }
+        }
+
+ 
+        if (playerScript_ex04.currentPlayerOnSwitch == 1)
+        {
+            facelessSwitch.GetComponent<SpriteRenderer>().color = new Color(0.8392158f, 0.2705882f, 0.2588235f);
+            openRedDoor2 = new Vector3(-10.41f, -11.95f, 0f);
+            if(redDoor2.transform.position.y < openRedDoor2.y)
+            {
+                redDoor2.transform.Translate(1 * Time.deltaTime * 1.1f, 0, 0);
+            }
+        }
+        if (playerScript_ex04.currentPlayerOnSwitch == 2)
+        {
+            facelessSwitch.GetComponent<SpriteRenderer>().color = new Color(0.7058824f, 0.6117647f, 0.2196079f);
+            if (yellowDoor2.transform.position.y < openRedDoor2.y)
+            {
+               yellowDoor2.transform.Translate(1 * Time.deltaTime * 1.1f, 0, 0);
+            }
+        }
+        if (playerScript_ex04.currentPlayerOnSwitch == 3)
+        {
+            facelessSwitch.GetComponent<SpriteRenderer>().color = new Color(0.145098f, 0.2392157f, 372549f);
+            openBlueDoor2 = new Vector3(-10.41f, -11.95f, 0f);
+            if (blueDoor2.transform.position.y < openRedDoor2.y)
+            {
+                blueDoor2.transform.Translate(1 * Time.deltaTime * 1.1f, 0, 0);
+            }
+        }
+
+
+    }
+
+
+}   
---
+++ d01_ex04/Assets/Script/playerScript_ex04.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript_ex04   : MonoBehaviour
+{
+    [SerializeField] private int id;
+    public static int flag_red;
+    public static int flag_yellow;
+    public static int flag_blue;
+    private int currentPlayer;
+    private Vector3 right;
+    private Vector3 left;
+    private Vector3 up;
+    [SerializeField] private float speed;
+    private Rigidbody2D rigidbody2d;
+
+    public static int whiteDoor;
+    public static bool redDoor1;
+    public static bool blueDoor1;
+    public static bool yellowDoor1;
+    public static int currentPlayerOnSwitch;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPlayerOnSwitch = 0;
+        yellowDoor1 = false;
+        blueDoor1 = false;
+        whiteDoor = 0;
+        redDoor1 = false;
+        flag_red = 0;
+        flag_yellow = 0;
+        flag_blue = 0;
+        currentPlayer = 1;
+        right = new Vector3(1, 0, 0);
+        left = new Vector3(-1, 0, 0);
+        up = new Vector3(0, 1, 0);
+        Physics2D.gravity = new Vector2(0, -3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Camera.main.transform.parent.name == "red")
+            currentPlayer = 1;
+        else if (Camera.main.transform.parent.name == "yellow")
+            currentPlayer = 2;
+        else if (Camera.main.transform.parent.name == "blue")
+            currentPlayer = 3;
+
+        if (id != currentPlayer)
+            gameObject.transform.GetComponent<Rigidbody2D>().mass = 1000;
+        else
+        {
+            gameObject.transform.GetComponent<Rigidbody2D>().mass = 5;
+            if (Input.GetKey("right"))
+                gameObject.transform.Translate(right * Time.deltaTime * speed);
+            if (Input.GetKey("left"))
+                gameObject.transform.Translate(left * Time.deltaTime * speed);
+            rigidbody2d = gameObject.transform.GetComponent<Rigidbody2D>();
+            if (rigidbody2d.velocity == new Vector2(0f, 0f) && Input.GetKeyDown("space"))
+                rigidbody2d.velocity = up * speed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (currentPlayer == 2 && other.name == "yellow_exit")
+            flag_yellow += 1;
+        if (currentPlayer == 1 && other.name == "red_exit")
+            flag_red += 1;
+        if (currentPlayer == 3 && other.name == "blue_exit")
+            flag_blue += 1;
+
+        if (other.CompareTag("teleportIn1"))
+        {
+            gameObject.transform.localPosition = entitiesScript.sTelepIn1.transform.localPosition;
+        }
+        else if (other.CompareTag("switchWhite"))
+        {
+            whiteDoor += 1;
+        }
+        else if (other.CompareTag("switchRed") && currentPlayer == 1)
+        {
+            redDoor1 = true;
+        }
+        else if (other.CompareTag("switchBlue") && currentPlayer == 3)
+        {
+            blueDoor1 = true;
+        }
+        else if (other.CompareTag("switchYellow") && currentPlayer == 2)
+        {
+            yellowDoor1 = true;
+        }
+        else if (other.CompareTag("switchFaceless"))
+        {
+            currentPlayerOnSwitch = currentPlayer;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if (collision.transform.name.Contains("Moving"))
+        {
+            gameObject.transform.SetParent(collision.collider.transform);   
+        } 
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.name.Contains("Moving"))
+        {
+            gameObject.transform.SetParent(null);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (currentPlayer == 1 && other.name == "red_exit")
+            flag_red -= 1;
+        if (currentPlayer == 2 && other.name == "yellow_exit")
+            flag_yellow -= 1;
+        if (currentPlayer == 3 && other.name == "blue_exit")
+            flag_blue -= 1;
+
+        if (other.CompareTag("switchWhite"))
+        {
+            whiteDoor -= 1;
+        }
+        if (other.CompareTag("switchRed") && currentPlayer == 1)
+        {
+            redDoor1 = false;
+        }
+        if (other.CompareTag("switchBlue") && currentPlayer == 3)
+        {
+            blueDoor1 = false;
+        }
+        if (other.CompareTag("switchYellow") && currentPlayer == 2)
+        {
+            yellowDoor1 = false;
+        }
+    }
+}
