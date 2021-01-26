a622648a75be2a55e8f2023bf2a77bbd64483378
THEOKOLESS <theophile.vitoux@gmail.com>
2021-01-26 20:02:51 +0100

    ex00 plus ou moins

---
+++ d02/ex00/Assets/ex00/Script/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _speed;
+    private Vector3 movePosition;
+    private Vector3 _moveDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _speed = 2f;
+        movePosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moveDir = (movePosition - transform.position);
+        if (_moveDir.magnitude > 0.01f)
+            transform.position += _moveDir.normalized * _speed * Time.deltaTime;
+    }
+    public  void    SetMovePosition(Vector3 movePosition)
+    {
+        this.movePosition = movePosition;
+    }
+}
---
+++ d02/ex00/Assets/ex00/Script/RightClickController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightClickController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if (Input.GetMouseButtonDown(1))
+        {
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPosition.z = 0f;
+          //  Debug.Log(GetComponent<PlayerController>());
+           // Debug.Log(worldPosition);
+            GetComponent<PlayerController>().SetMovePosition(worldPosition);
+        
+        }   
+    }
+}
