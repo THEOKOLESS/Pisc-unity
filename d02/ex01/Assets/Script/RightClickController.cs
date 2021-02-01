4e1db7eb9bb20924b54b95e6a5f9b12a90f4d001
THEOKOLESS <theophile.vitoux@gmail.com>
2021-02-01 16:58:40 +0100

    ex01 lol

---
+++ d02/ex01/Assets/Script/RightClickController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightClickController : MonoBehaviour
+{
+    private Vector3 worldPosition;
+    private List<Footman> selectedFootmanList;
+    private Footman footman;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        selectedFootmanList = new List<Footman>();
+    }
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown(1))
+        {
+            worldPosition.z = 0f;
+            GetComponent<PlayerController>().SetMovePosition(worldPosition);
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D collider2D = Physics2D.OverlapPoint(worldPosition, LayerMask.GetMask("Footman"));
+            selectedFootmanList.Clear();
+            if (collider2D != null)
+            {
+                footman = collider2D.GetComponent<Footman>();
+                if (footman != null)
+                {
+                    selectedFootmanList.Add(footman);
+                }
+            }
+                Debug.Log(selectedFootmanList.Count);
+        }
+
+    }
+}
+
---
+++ d02/ex01/Assets/Script/SetVelocity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security;
+using UnityEditor;
+using UnityEngine;
+
+public class SetVelocity : MonoBehaviour, ISetVelocity
+{
+    [SerializeField] private float speed;
+
+    private Vector3 velocityVector;
+    private Rigidbody2D rigidbody2D;
+
+    private void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    public  void SetVelocityVec(Vector3 velocityVector)
+    {
+        this.velocityVector = velocityVector;
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody2D.velocity = velocityVector * speed;
+    }
+}
