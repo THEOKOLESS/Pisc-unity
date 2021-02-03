1794c30dc804a2bee52b4967fd9b68d6f9753b1b
Abey Cultivey <AbeyCultivey@MacBook-Pro-de-Josephine.local>
2021-02-03 18:35:14 +0100

    setup ex02

---
+++ d02/_d02/Assets/Script/Ex01/Footman.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footman : MonoBehaviour
+{
+    [SerializeField] private GameObject selectedRing;
+    private PlayerController movePos;
+
+    private void Awake()
+    {
+        movePos = GetComponent<PlayerController>();
+        SetSelectedVisible(false);
+    }
+
+    public  void    SetSelectedVisible(bool visible)
+    {
+        selectedRing.SetActive(visible);
+    }
+
+    public  void    MoveTo(Vector3 targetPos)
+    {
+        movePos.SetMovePosition(targetPos);
+    }
+}
---
+++ d02/_d02/Assets/Script/Ex02/SetMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ex02
+{
+
+    public class SetMove : MonoBehaviour
+    {
+        private Vector3 _movePos;
+        private float _speed;
+        // Start is called before the first frame update
+        void Start()
+        {
+            _speed = 3f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position += _movePos * _speed * Time.deltaTime;
+        }
+
+        public void SetMovePos(Vector3 _movePos)
+        {
+            this._movePos = _movePos;
+        }
+    }
+}
+    
