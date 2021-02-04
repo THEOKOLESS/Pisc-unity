5153424c06f9b214dedb0dfcbe0d266ee30a84ef
Abey Cultivey <AbeyCultivey@MacBook-Pro-de-Josephine.local>
2021-02-04 17:50:49 +0100

    ex02 done

---
+++ d02/ex01/Assets/Script/Ex02/Footman/Footman.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ex02
+{
+    public class Footman : MonoBehaviour
+    {
+        [SerializeField] private GameObject selectedRing;
+        private PlayerController movePos;
+
+        private void Awake()
+        {
+            movePos = GetComponent<PlayerController>();
+            SetSelectedVisible(false);
+           
+        }
+
+        private void OnEnable()
+        {
+            MoveTo(new Vector3(-3.7f, -0.9f, 0));
+        }
+
+
+        public void SetSelectedVisible(bool visible)
+        {
+            selectedRing.SetActive(visible);
+        }
+
+        public void MoveTo(Vector3 targetPos)
+        {
+            movePos.SetMovePosition(targetPos);
+        }
+    }
+}
+
