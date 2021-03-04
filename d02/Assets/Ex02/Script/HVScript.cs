192e3e8aa1cf0ba2ab6123f30b6104dec7fdc02d
Theophile Vitoux <tvitoux@student.42.fr>
2021-03-04 15:44:45 +0100

    d02Done

---
+++ d02/Assets/Ex02/Script/HVScript.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ex02
+{
+    public class HVScript : MonoBehaviour
+    {
+       [SerializeField] private int hp;
+    }
+}
